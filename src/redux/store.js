1bf2debd7b19855f80f42863e432f1633ac1e6f5
DimaHavr <109313387+DimaHavr@users.noreply.github.com>
2023-06-02 17:42:17 +0300

    add auth-state

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,5 +1,28 @@
 import { configureStore } from '@reduxjs/toolkit';
+import {
+  persistStore,
+  FLUSH,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+  REHYDRATE,
+} from 'redux-persist';
+import { persistedAuthSlice } from '../redux/auth/authSlice';
 
 export const store = configureStore({
-  reducer: {},
+  reducer: {
+    auth: persistedAuthSlice,
+  },
+
+  middleware(getDefaultMiddleware) {
+    return getDefaultMiddleware({
+      serializableCheck: {
+        ignoreActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    });
+  },
+  devTools: process.env.NODE_ENV === 'development',
 });
+
+export const persistor = persistStore(store);
