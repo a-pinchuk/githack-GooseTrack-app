2a9da408fdd762fc1939eda6ec4117bbc3f7b032
VozzRoman <looper0500@ukr.net>
2023-06-04 22:05:18 +0300

    add taskColumn

---
+++ src/components/TasksCopmonents/AddTasksBth/AddTasksBthStyled.js
@@ -0,0 +1,40 @@
+import styled from "styled-components";
+import { theme } from "theme";
+
+export const ButtonTask = styled.button`
+cursor: pointer;
+display: flex;
+align-items: center;
+justify-content: center;
+width: 100%;
+padding: 12px;
+font-family: ${theme.fonts.text};
+font-weight: ${theme.fontWeights.medium};
+background: ${theme.colors.lightBlue};
+color: ${theme.colors.black};
+font-size: ${theme.fontSizes.s};
+height: 47px;
+line-height: ${theme.lineHeights.body};
+border: 1px dashed ${theme.colors.primary};
+border-radius: ${theme.radii.small};
+transition: background 250ms linear;
+&:hover{
+	background-color: ${theme.colors.primary};
+	color: ${theme.colors.white};
+}
+
+
+span{
+	display: block;
+	align-self: center;
+	margin-right: 8px;
+	margin-bottom: 3px;
+	font-size: 30px;
+	font-weight: 400;
+}
+`;
+export const ButtonBox = styled.div`
+padding: 32px 18px 28px 18px;
+
+`
+
