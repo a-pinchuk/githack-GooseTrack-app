e4a702fb9dccf9332edca26b9178ff492025d2bc
Katya-Semeniuk <katyasemeniuk@gmail.com>
2023-06-05 00:09:56 +0300

    avatar input

--- src/components/UserForm/UserForm.jsx
+++ src/components/UserForm/UserForm.jsx
@@ -1,9 +1,34 @@
 import { useDispatch, useSelector } from 'react-redux';
 import { useFormik } from 'formik';
-import { Box, Button, TextField, Typography } from '@mui/material';
 import { updateUserInfo } from '../../redux/auth/operations'; // Импорт вашего thunk
 import { selectUser } from 'redux/auth/selectors';
 import moment from 'moment/moment';
+import * as Yup from 'yup';
+import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
+import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
+import { DatePicker } from '@mui/x-date-pickers/DatePicker';
+import {
+  Container,
+  FormContainer,
+  StyledAvatar,
+  Plus,
+  AvatarInput,
+  StyledInput,
+  Button,
+  Label,
+} from './UserForm.styled';
+
+const schema = Yup.object().shape({
+  avatar: Yup.string().url(),
+  name: Yup.string()
+    .min(2, 'Too short')
+    .max(16, 'No more than 16 symbols')
+    .required('Required'),
+  birthday: Yup.date().default(() => new Date()),
+  email: Yup.string().email('Invalid email').required('Required'),
+  phone: Yup.string().matches(/^\+380\d{2} \d{7}$/, 'Invalid phone number'),
+  skype: Yup.string().max(16, 'No more than 16 symbols'),
+});
 const UserForm = () => {
   const dispatch = useDispatch();
   const user = useSelector(selectUser);
@@ -17,6 +42,7 @@ const UserForm = () => {
       skype: user.skype,
       birthday: user.birthday,
     },
+    validationSchema: schema,
     onSubmit: values => {
       const formattedDate = moment(values.birthday).format('DD/MM/YYYY'); // Преобразуйте дату в нужный формат
 
@@ -30,60 +56,92 @@ const UserForm = () => {
   });
 
   return (
-    <Box
-      component="form"
-      onSubmit={formik.handleSubmit}
-      sx={{ display: 'flex', flexDirection: 'column', gap: '16px' }}
-    >
-      <Typography variant="h4">Update User Info</Typography>
-      <input
-        id="avatar"
-        name="avatar"
-        type="file"
-        onChange={event => {
-          formik.setFieldValue('avatar', event.currentTarget.files[0]);
-        }}
-      />
-      <TextField
-        id="name"
-        name="name"
-        label="Name"
-        value={formik.values.name}
-        onChange={formik.handleChange}
-      />
-      <TextField
-        id="email"
-        name="email"
-        label="Email"
-        value={formik.values.email}
-        onChange={formik.handleChange}
-      />
-      <TextField
-        id="phone"
-        name="phone"
-        label="Phone"
-        value={formik.values.phone}
-        onChange={formik.handleChange}
-      />
-      <TextField
-        id="skype"
-        name="skype"
-        label="Skype"
-        value={formik.values.skype}
-        onChange={formik.handleChange}
-      />
-      <TextField
-        id="birthday"
-        name="birthday"
-        type="date"
-        label="Birthday"
-        value={formik.values.birthday}
-        onChange={formik.handleChange}
-      />
-      <Button type="submit" variant="contained" color="primary">
-        Submit
-      </Button>
-    </Box>
+    <Container>
+      <FormContainer component="form" onSubmit={formik.handleSubmit}>
+        <StyledAvatar />
+        <Plus />
+        <AvatarInput
+          id="avatar"
+          name="avatar"
+          type="file"
+          onChange={event => {
+            formik.setFieldValue('avatar', event.currentTarget.files[0]);
+          }}
+        />
+        <Label>User Name</Label>
+        <StyledInput
+          id="name"
+          name="name"
+          label="Name"
+          placeholder="Skype"
+          value={formik.values.name || ''}
+          onChange={formik.handleChange}
+          onBlur={formik.handleBlur}
+          error={formik.touched.name && formik.errors.name ? true : false}
+          helperText={
+            (formik.touched.name && formik.errors.name) ||
+            (formik.values.name.length === 16 ? 'Only 16 symbols' : null)
+          }
+        />
+        <Label>Birthday</Label>
+        <StyledInput
+          id="email"
+          name="email"
+          label="Email"
+          value={formik.values.email || ''}
+          onChange={formik.handleChange}
+          error={formik.touched.email && formik.errors.email ? true : false}
+          helperText={formik.touched.email && formik.errors.email}
+        />
+        {/* <Label>Birthday</Label>
+        <LocalizationProvider dateAdapter={AdapterDayjs}>
+          <DatePicker
+            value={
+              formik.values.birthday ? moment(formik.values.birthday) : moment()
+            }
+            onChange={value => formik.setFieldValue('birthday', value)}
+            renderInput={params => (
+              <StyledInput
+                {...params}
+                id="birthday"
+                name="birthday"
+                type="date"
+                label="Birthday"
+                value={
+                  formik.values.birthday
+                    ? moment(formik.values.birthday)
+                    : moment()
+                }
+                onChange={formik.handleChange}
+              />
+            )}
+          />
+        </LocalizationProvider> */}
+
+        <Label>Phone</Label>
+        <StyledInput
+          id="phone"
+          placeholder=" 38 (097)..."
+          name="phone"
+          value={formik.values.phone || ''}
+          onChange={formik.handleChange}
+          onBlur={formik.handleBlur}
+          error={formik.touched.phone && formik.errors.phone ? true : false}
+          helperText={formik.touched.phone && formik.errors.phone}
+        />
+        <Label>Skype</Label>
+        <StyledInput
+          placeholder="Skype"
+          id="skype"
+          name="skype"
+          value={formik.values.skype || ''}
+          onChange={formik.handleChange}
+          error={formik.touched.skype && formik.errors.skype ? true : false}
+          helperText={formik.touched.skype && formik.errors.skype}
+        />
+        <Button type="submit">Save changes</Button>
+      </FormContainer>
+    </Container>
   );
 };
 export default UserForm;
--- src/components/UserForm/UserForm.styled.js
+++ src/components/UserForm/UserForm.styled.js
@@ -1,20 +1,107 @@
 import styled from 'styled-components';
+import { ReactComponent as Avatar } from '../../images/avatar.svg';
+import { ReactComponent as Cross } from '../../images/cross.svg';
+import { Box } from '@mui/material';
+
+export const Container = styled.div`
+  width: 704px;
+  height: 854px;
+  margin-left: 20px;
+  margin-right: 20px;
+  margin-bottom: 40px;
+  
+  background-color: ${p => p.theme.colors.white};
+   @media (min-width: 768px) {
+    margin-left: 32px;
+  margin-right: 32px;
+  margin-bottom: 38px;
+  }
+  @media (min-width: 1440px) {
+    min-width: 1440px;
+`;
+export const FormContainer = styled(Box)`
+  display: flex;
+  flex-direction: column;
+  min-width: 299 px;
+  background-color: ${p => p.theme.colors.white};
+
+  @media (min-width: 768px) {
+    min-width: 354px;
+  }
+  @media (min-width: 1440px) {
+    min-width: 1440px;
+  }
+`;
+export const StyledAvatar = styled(Avatar)`
+  width: 124px;
+  height: 124px;
+  fill: rgba(62, 133, 243, 0.18);
+  border: 2px solid #3e85f3;
+  border-radius: ${p => p.theme.radii.round};
+`;
+
+export const Plus = styled(Cross)`
+  font-size: 15px;
+  &:hover,
+  &:focus {
+    fill: red;
+  }
+`;
+
+export const AvatarInput = styled.input`
+  font-size: 24px;
+`;
+
+export const Label = styled.label`
+  font-family: ${p => p.theme.fonts.text};
+  font-size: ${p => p.theme.fontSizes.xs};
+  font-weight: 400;
+  line-height: ${p => p.theme.lineHeights.heading};
+  color: ${p => p.theme.colors.black};
+  &:not(:last-child) {
+    margin-bottom: 8px;
+  }
+`;
 
 export const StyledInput = styled.input`
-  font-size: 40px;
+  border-radius: ${p => p.theme.fontSizes.s};
+  border: 1px solid #11111126;
+  height: 42px;
+  outline: none;
+  padding: 14px 18px;
+  &:not(:last-child) {
+    margin-bottom: 24px;
+  }
+  &:hover,
+  &:focus {
+    border: 1px solid #11111126;
+  }
+  @media (min-width: 768px) {
+    height: 46px;
+  }
+
+  @media (min-width: 1440px) {
+    min-width: 1440px;
+  }
 `;
 
-// export const Form = styled(Form)`
-//   font-size: 40px;
-// `;
-export const ErrorText = styled.div`
-  color: red;
-`;
-
-export const PictureBox = styled.div`
-  color: red;
-  fill: red;
-  svg {
-    fill: red;
+export const Button = styled.button`
+  margin-left: auto;
+  margin-right: auto;
+  width: 262px;
+  height: 48px;
+  font-family: ${p => p.theme.fonts.text};
+  font-weight: ${p => p.theme.fontWeights.medium};
+  line-height: ${p => p.theme.lineHeights.body};
+  font-size: ${p => p.theme.fontSizes.s};
+  text-align: center;
+  color: ${p => p.theme.colors.white};
+  background-color: ${p => p.theme.colors.primary};
+  border-radius: ${p => p.theme.radii.big};
+  border: ${p => p.theme.borders.none};
+  cursor: pointer;
+  &:hover,
+  &:focus {
+    background-color: ${p => p.theme.colors.secondary};
   }
 `;
