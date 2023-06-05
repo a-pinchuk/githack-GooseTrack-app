96ce7f2047372e83b64fadcfdd0af90c9498fbf1
VozzRoman <looper0500@ukr.net>
2023-06-06 00:01:40 +0300

    adding

--- src/components/CalendarPage/CalendarPage.jsx
+++ src/components/CalendarPage/CalendarPage.jsx
@@ -1,6 +1,7 @@
 // import React, { useEffect, useState } from 'react';
 import LogoutBtn from 'components/TestBtnLogout/LogoutBtn';
 import { TemporaryHeaderCalendar } from './TemporaryHeaderCalendar';
+
 // import { useDispatch, useSelector } from 'react-redux';
 // import { selectAllTasks } from 'redux/task/selectors';
 //import {} from './CalendarPage.styled';
--- src/components/TasksCopmonents/TasksCopmonents/AddTasksBth/AddTasksBth.jsx
+++
@@ -1,9 +0,0 @@
-import { ButtonBox, ButtonTask } from "./AddTasksBthStyled"
-
-export const AddTasksBth = () => {
-	return(
-		<ButtonBox>
-		<ButtonTask><span>+</span>Add task</ButtonTask>
-		</ButtonBox>
-	)
-}
