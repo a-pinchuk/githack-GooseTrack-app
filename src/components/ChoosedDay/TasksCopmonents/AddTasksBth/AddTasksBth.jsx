2b5fd30d78d73590ea09c0d794f677618cb3a688
VozzRoman <looper0500@ukr.net>
2023-06-06 18:57:17 +0300

    adding

--- src/components/ChoosedDay/TasksCopmonents/AddTasksBth/AddTasksBth.jsx
+++ src/components/ChoosedDay/TasksCopmonents/AddTasksBth/AddTasksBth.jsx
@@ -1,9 +1,20 @@
-import { ButtonBox, ButtonTask } from "./AddTasksBthStyled"
+import { useState } from 'react';
+import { ButtonBox, ButtonTask } from './AddTasksBthStyled';
+import { Modal } from 'components/Modal/Modal';
 
 export const AddTasksBth = () => {
-	return(
-		<ButtonBox>
-		<ButtonTask><span>+</span>Add task</ButtonTask>
-		</ButtonBox>
-	)
-}
+  const [modal, setModal] = useState(false);
+  const handleOpenModal = () => {
+    setModal(true);
+  };
+  return (
+    <>
+      <ButtonBox onClick={handleOpenModal}>
+        <ButtonTask>
+          <span>+</span>Add task
+        </ButtonTask>
+      </ButtonBox>
+      {modal && <Modal />}
+    </>
+  );
+};
