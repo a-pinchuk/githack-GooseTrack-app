908852542db759567f9c2e5c87929948612f54d4
Sasha92Shevchuk <108683139+Sasha92Shevchuk@users.noreply.github.com>
2023-06-07 16:57:25 +0300

    fix worning and code

--- src/components/CalendarPage/CalendarPage.jsx
+++ src/components/CalendarPage/CalendarPage.jsx
@@ -1,7 +1,7 @@
-import React, { useEffect, useState, Suspense } from 'react';
+import React, { useEffect, Suspense } from 'react';
 import { Outlet } from 'react-router-dom';
 import LogoutBtn from 'components/TestBtnLogout/LogoutBtn';
-import { useNavigate } from 'react-router';
+import { useNavigate, useParams } from 'react-router';
 import { useLocation } from 'react-router-dom';
 import moment from 'moment';
 
@@ -14,90 +14,62 @@ import { fetchAllTasks } from 'redux/task/operations';
 import { selectAllTasks } from 'redux/task/selectors';
 
 const CalendarPage = () => {
-  const [today, setToday] = useState(moment());
-  //console.log('CalendarPage ~ today:', today.format('YYYY-MM-DD'));
-  const [typeSelect, setTypeSelect] = useState('month');
-  console.log('CalendarPage ~ typeSelect:', typeSelect);
   const navigate = useNavigate();
   const dispatch = useDispatch();
+
+  const { currentDay, currentDate } = useParams();
   const { pathname } = useLocation();
+  const typeSelect = pathname.includes('/calendar/day') ? 'day' : 'month';
 
+  const workDate = currentDate || currentDay;
+
+  // * Task
   const tasks = useSelector(selectAllTasks);
-
-  const pathWithoutDate = pathname.slice(0, pathname.lastIndexOf('/'));
 
   useEffect(() => {
     if (pathname === '/calendar') {
       navigate(`/calendar/month/${moment().format('YYYY-MM-DD')}`);
     }
-    // if (pathWithoutDate === '/calendar/day') {
-    //   setTypeSelect('day');
-    // }
-
-    if (tasks.length === 0) {
-      dispatch(fetchAllTasks());
-    }
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [dispatch]);
+  }, [pathname, navigate]);
 
   useEffect(() => {
-    if (pathWithoutDate === '/calendar/day') {
-      setTypeSelect('day');
+    if (tasks.length === 0) {
+      // ! Якщо змінився місяць то треба фетчити таски
+      //Винести в окремий
+      dispatch(fetchAllTasks());
     }
-  }, [pathWithoutDate]);
-
-  //const startDay = today.clone().startOf('month').startOf('week');
-  // console.log('CalendarPage ~ startDay:', startDay);
-  //const endDay = today.clone().endOf('month').endOf('week');
+  }, [dispatch, tasks]);
 
   const todayHandler = () => {
-    setToday(moment());
+    // Перейти на день
+    console.log('Перейти на день');
+    // setToday(moment());
   };
 
   const prevHandler = () => {
-    if (typeSelect === 'month') {
-      setToday(prev => {
-        const prevMonth = prev.clone().subtract(1, 'month');
-        navigate(`/calendar/month/${prevMonth.format('YYYY-MM-DD')}`);
-        return prevMonth;
-      });
-      return;
-    }
-    setToday(prev => {
-      const prevDay = prev.clone().subtract(1, 'day');
-      navigate(`/calendar/day/${prevDay.format('YYYY-MM-DD')}`);
-      return prevDay;
-    });
+    navigate(
+      `/calendar/${typeSelect}/${moment(workDate)
+        .subtract(1, typeSelect)
+        .format('YYYY-MM-DD')}`
+    );
   };
 
   const nextHandler = () => {
-    if (typeSelect === 'month') {
-      setToday(prev => {
-        const nextMonth = prev.clone().add(1, 'month');
-        navigate(`/calendar/month/${nextMonth.format('YYYY-MM-DD')}`);
-        return nextMonth;
-      });
-      return;
-    }
-    setToday(prev => {
-      const nextDay = prev.clone().add(1, 'day');
-      navigate(`/calendar/day/${nextDay.format('YYYY-MM-DD')}`);
-      return nextDay;
-    });
+    navigate(
+      `/calendar/${typeSelect}/${moment(workDate)
+        .add(1, typeSelect)
+        .format('YYYY-MM-DD')}`
+    );
   };
 
   const typeMonthHendler = () => {
-    const desiredPath = `/calendar/month/${moment().format('YYYY-MM-DD')}`;
-    if (pathname !== desiredPath) {
-      setTypeSelect('month');
-      navigate(desiredPath);
-    }
+    navigate(`/calendar/month/${moment().format('YYYY-MM-DD')}`);
   };
 
   const typeDayHendler = () => {
-    setTypeSelect('day');
     navigate(`/calendar/day/${moment().format('YYYY-MM-DD')}`);
   };
+
   return (
     <div
       style={{
@@ -111,7 +83,7 @@ const CalendarPage = () => {
 
       <TemporaryHeaderCalendar />
       <CalendarToolbar
-        today={today}
+        today={moment(workDate)}
         typeSelect={typeSelect}
         todayHandler={todayHandler}
         prevHandler={prevHandler}
