91ca75f04846d24ec9ccc7772b6f8e746094062f
a.pinchuk <co.born2code@gmail.com>
2023-06-08 12:05:33 +0200

    userinfo fix

--- src/components/Header/UserInfo/UserInfo.jsx
+++ src/components/Header/UserInfo/UserInfo.jsx
@@ -1,35 +1,41 @@
-import { HeaderUserInfoSection, HeaderUserName, HeaderUserPhoto, TextAvatar, AvatorTaskList } from './UserInfo.style';
-import userPhoto from '../image/Sample_User_Icon.png';
+import {
+  HeaderUserInfoSection,
+  HeaderUserName,
+  HeaderUserPhoto,
+  TextAvatar,
+  //   AvatorTaskList,
+} from './UserInfo.style';
+// import userPhoto from '../image/Sample_User_Icon.png';
 import { useSelector } from 'react-redux';
 import { selectUser } from 'redux/auth/selectors';
 
 const UserInfo = () => {
-    const user = useSelector(selectUser);
+  const user = useSelector(selectUser);
 
-    const avaFunc = value => {
-        if (!value) {
-          return;
-        } else {
-          return value.slice(0, 1).toUpperCase();
-        }
-    };
+  const avaFunc = value => {
+    if (!value) {
+      return;
+    } else {
+      return value.slice(0, 1).toUpperCase();
+    }
+  };
 
-    return (
-        <HeaderUserInfoSection>
-            <HeaderUserName>{user.name}</HeaderUserName>
+  return (
+    <HeaderUserInfoSection>
+      <HeaderUserName>{user.name}</HeaderUserName>
 
-            {!user?.avatarUrl ? (
-                <TextAvatar>{avaFunc(user?.name)}</TextAvatar>
-            ) : (
-                <HeaderUserPhoto
-                   src={user?.avatarUrl}
-                   alt="avator"
-                   width="28"
-                   height="28"
-                />
-            )}
-        </HeaderUserInfoSection>
-    )
+      {!user?.avatarUrl ? (
+        <TextAvatar>{avaFunc(user?.name)}</TextAvatar>
+      ) : (
+        <HeaderUserPhoto
+          src={user?.avatarUrl}
+          alt="avator"
+          width="28"
+          height="28"
+        />
+      )}
+    </HeaderUserInfoSection>
+  );
 };
 
 export default UserInfo;
