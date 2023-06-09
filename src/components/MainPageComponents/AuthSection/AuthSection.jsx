314fcf08dd0ab988bccbbd0d80f3f4887a5fb763
VitaliiSmyrnov <vitaliy.smirnov@ukr.net>
2023-06-09 23:38:19 +0300

    almost done except mobile version

--- src/components/MainPageComponents/AuthSection/AuthSection.jsx
+++ src/components/MainPageComponents/AuthSection/AuthSection.jsx
@@ -1,7 +1,8 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 
 import {
   Header,
+  ContentWrapper,
   ImageWrapper,
   LoginLink,
   RegisterLink,
@@ -13,29 +14,46 @@ import goose2 from 'images/mainPage/desktop/desktop_goose_mainPage@2x.png';
 import sprite from 'icons/sprite.svg';
 
 export const AuthSection = () => {
-  return (
-    <Header>
-      <ImageWrapper>
-        <img
-          srcSet={`${goose1} 1x, ${goose2} 2x`}
-          src={goose1}
-          alt="Goose welcome you"
-          title="Goose welcome you"
-        />
-      </ImageWrapper>
+  const [viewportHeight, setViewportHeight] = useState(window.innerHeight);
+  console.log(viewportHeight);
 
-      <h1>
-        G<span>oo</span>seTrack
-      </h1>
-      <nav>
-        <LoginLink to="login">
-          Log in
-          <svg height="13.5" width="13.5">
-            <use href={sprite + '#icon-enter'}></use>
-          </svg>
-        </LoginLink>
-        <RegisterLink to="register">Sign up</RegisterLink>
-      </nav>
+  useEffect(() => {
+    const updateViewportHeight = () => {
+      setViewportHeight(window.innerHeight);
+    };
+
+    window.addEventListener('resize', updateViewportHeight);
+
+    return () => {
+      window.removeEventListener('resize', updateViewportHeight);
+    };
+  }, []);
+
+  return (
+    <Header height={viewportHeight}>
+      <ContentWrapper height={viewportHeight}>
+        <ImageWrapper height={viewportHeight}>
+          <img
+            srcSet={`${goose1} 1x, ${goose2} 2x`}
+            src={goose1}
+            alt="Goose welcome you"
+            title="Goose welcome you"
+          />
+        </ImageWrapper>
+
+        <h1>
+          G<span>oo</span>seTrack
+        </h1>
+        <nav>
+          <LoginLink to="login">
+            Log in
+            <svg height="13.5" width="13.5">
+              <use href={sprite + '#icon-enter'}></use>
+            </svg>
+          </LoginLink>
+          <RegisterLink to="register">Sign up</RegisterLink>
+        </nav>
+      </ContentWrapper>
     </Header>
   );
 };
