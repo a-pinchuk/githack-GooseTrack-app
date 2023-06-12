bc6135f6f3fc9018d365e7ff19db75e1f6bf69c5
VitaliiSmyrnov <vitaliy.smirnov@ukr.net>
2023-06-12 13:40:32 +0300

    scroll start

---
+++ src/components/MainPageComponents/ScrollToTop/ScrollToTop.jsx
@@ -0,0 +1,34 @@
+import React from 'react';
+
+import { Container } from './ScrollToTop.styled';
+
+export const ScrollToTop = () => {
+  let calcScrollValue = () => {
+    let scrollProgress = document.getElementById('scroll-progress');
+    let progressValue = document.getElementById('scroll-progress-value');
+    let pos = document.documentElement.scrollTop;
+    let calcHeight =
+      document.documentElement.scrollHeight -
+      document.documentElement.clientHeight;
+    let scrollValue = Math.round((pos * 100) / calcHeight);
+    if (pos > 100) {
+      scrollProgress.style.display = 'grid';
+    } else {
+      scrollProgress.style.display = 'none';
+    }
+    scrollProgress.addEventListener('click', () => {
+      document.documentElement.scrollTop = 0;
+    });
+    scrollProgress.style.background = `conic-gradient(#03cc65 ${scrollValue}%, #d7d7d7 ${scrollValue}%)`;
+  };
+  window.onscroll = calcScrollValue;
+  window.onload = calcScrollValue;
+
+  return (
+    <Container>
+      <div id="scroll-progress">
+        <span id="scroll-progress-value">&#x1F815;</span>
+      </div>
+    </Container>
+  );
+};
--- src/components/MainPageComponents/index.js
+++ src/components/MainPageComponents/index.js
@@ -1,3 +1,4 @@
 export * from 'components/MainPageComponents/AuthSection/AuthSection';
 export * from 'components/MainPageComponents/Description/Description';
 export * from 'components/MainPageComponents/ReviewsSlider/ReviewsSlider';
+export * from 'components/MainPageComponents/ScrollToTop/ScrollToTop';
